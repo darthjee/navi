b835647f74f4f9c7684a107c9ac8e1589b75c469
darthjee <darthjee@gmail.com>
2026-03-01 13:06:41 -0300

    Lint fix

--- source/spec/ResourceRequest_spec.js
+++ source/spec/ResourceRequest_spec.js
@@ -16,33 +16,33 @@ describe('ResourceRequest', () => {
     await expectAsync(resourceRequest.check()).toBeResolvedTo(true);
   });
 
-  describe("when request status is not a match", () => {
+  describe('when request status is not a match', () => {
     beforeEach(() => {
       expectedError = jasmine.objectContaining({
-            name: 'RequestFailed',
-            statusCode: 404,
-            url,
-        })
+        name: 'RequestFailed',
+        statusCode: 404,
+        url,
+      });
     });
     
     it('throws RequestFailed when status does not match', async () => {
-        spyOn(axios, 'get').and.returnValue(Promise.resolve({ status: 404 }));
-        await expectAsync(resourceRequest.check()).toBeRejectedWith(expectedError);
+      spyOn(axios, 'get').and.returnValue(Promise.resolve({ status: 404 }));
+      await expectAsync(resourceRequest.check()).toBeRejectedWith(expectedError);
     });
   });
 
-  describe("when request is 5xx", () => {
+  describe('when request is 5xx', () => {
     beforeEach(() => {
       expectedError = jasmine.objectContaining({
-            name: 'RequestFailed',
-            statusCode: 500,
-            url,
-        })
+        name: 'RequestFailed',
+        statusCode: 500,
+        url,
+      });
     });
 
     it('throws RequestFailed with correct status and url on error.response', async () => {
-        spyOn(axios, 'get').and.returnValue(Promise.reject({ response: { status: 500 } }));
-        await expectAsync(resourceRequest.check()).toBeRejectedWith(expectedError);
+      spyOn(axios, 'get').and.returnValue(Promise.reject({ response: { status: 500 } }));
+      await expectAsync(resourceRequest.check()).toBeRejectedWith(expectedError);
     });
   });
 });
