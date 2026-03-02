84a3601ae305c8dbe2070fa34b6cb009590902eb
darthjee <darthjee@gmail.com>
2026-03-02 20:16:57 -0300

    Add docs

--- source/lib/models/ResourceRequest.js
+++ source/lib/models/ResourceRequest.js
@@ -1,4 +1,12 @@
+/**
+ * ResourceRequest represents a request to a specific URL with an expected status code.
+ * @author darthjee
+ */
 class ResourceRequest {
+  /**
+   * @param {string} url The URL to request
+   * @param {number} status The expected status code of the response
+   */
   constructor({ url, status }) {
     this.url = url;
     this.status = status;
