acf6d4774d7a2ea7d9e6ef2543b7e41851892734
Fernando Favini <darthjee@gmail.com>
2026-03-02 14:44:07 -0300

    Split class (#11)

--- source/lib/models/ResourceRequest.js
+++ source/lib/models/ResourceRequest.js
@@ -1,30 +1,7 @@
-import axios from 'axios';
-import { RequestFailed } from '../exceptions/RequestFailed.js';
-
 class ResourceRequest {
   constructor({ url, status }) {
     this.url = url;
     this.status = status;
-  }
-
-  async check() {
-    try {
-      return await this.#request();
-    } catch (error) {
-      if (error.response) {
-        throw new RequestFailed(error.response.status, this.url);
-      }
-      throw error;
-    }
-  }
-
-  async #request() {
-    const response = await axios.get(this.url);
-    if (response.status === this.status) {
-      return true;
-    } else {
-      throw new RequestFailed(response.status, this.url);
-    }
   }
 }
 
---
+++ source/lib/services/Requester.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { RequestFailed } from '../exceptions/RequestFailed.js';
+
+class Requester {
+  constructor(resourceRequest) {
+    this.resourceRequest = resourceRequest;
+  }
+
+  async perform() {
+    try {
+      return await this.#request();
+    } catch (error) {
+      if (error.response) {
+        throw new RequestFailed(error.response.status, this.resourceRequest.url);
+      }
+      throw error;
+    }
+  }
+
+  async #request() {
+    const response = await axios.get(this.resourceRequest.url);
+    if (response.status === this.resourceRequest.status) {
+      return true;
+    } else {
+      throw new RequestFailed(response.status, this.resourceRequest.url);
+    }
+  }
+}
+
+export { Requester };
+
+
