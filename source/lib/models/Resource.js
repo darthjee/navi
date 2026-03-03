6bba28f5ad9120462652e3837ad0cffbf4c83ed0
darthjee <darthjee@gmail.com>
2026-03-02 21:28:48 -0300

    Add resource

---
+++ source/lib/models/Resource.js
@@ -0,0 +1,16 @@
+/**
+ * Resource class represents a resource with a name and associated resource requests.
+ * @author darthjee
+ */
+class Resource {
+  /**
+   * Represents a resource with a name and associated resource requests.
+   * @param {object} params - The parameters for creating a Resource instance.
+   * @param {string} params.name - The name of the resource.
+   * @param {Array} params.resourceRequests - The resource requests associated with the resource.
+   */
+  constructor({ name, resourceRequests }) {
+    this.name = name;
+    this.resourceRequests = resourceRequests;
+  }
+}
