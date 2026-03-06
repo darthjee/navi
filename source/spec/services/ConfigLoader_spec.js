6332e7c55293e5716d8690fa3998ee1fd959a6ea
darthjee <darthjee@gmail.com>
2026-03-06 20:42:19 -0300

    Add scenario

--- source/spec/services/ConfigLoader_spec.js
+++ source/spec/services/ConfigLoader_spec.js
@@ -4,24 +4,43 @@ import { Resource } from '../../lib/models/Resource.js';
 import { ConfigLoader } from '../../lib/service/configLoader.js';
 
 describe('ConfigLoader', () => {
+  let expectedConfig;
+  let expectedResources;
+  let expectedResourceRequests;
+
   describe('.fromFile', () => {
-    it('returns mapped resources by name', () => {
-      const expectedResourceRequests = [
-        new ResourceRequest({ url: '/categories.json', status: 200 })
-      ];
-      const expectedResources = {
-        categories: new Resource({
-          name: 'categories', resourceRequests: expectedResourceRequests
-        }),
-      };
-      const expectedConfig = { resources: expectedResources };
+    describe('when the yaml file is valid', () => {
+      beforeEach(() => {
+        expectedResourceRequests = [
+          new ResourceRequest({ url: '/categories.json', status: 200 })
+        ];
+        expectedResources = {
+          categories: new Resource({
+            name: 'categories', resourceRequests: expectedResourceRequests
+          }),
+        };
+        expectedConfig = { resources: expectedResources };
+      });
 
-      const file = '../fixtures/config/sample_config.yml';
-      const configFilePath = fileURLToPath(new URL(file, import.meta.url));
+      it('returns mapped resources by name', () => {
+        const file = '../fixtures/config/sample_config.yml';
+        const configFilePath = fileURLToPath(new URL(file, import.meta.url));
 
-      const resources = ConfigLoader.fromFile(configFilePath);
+        const resources = ConfigLoader.fromFile(configFilePath);
 
-      expect(resources).toEqual(expectedConfig);
+        expect(resources).toEqual(expectedConfig);
+      });
+    });
+
+    describe('when the yaml file does not contain resources key', () => {
+      it('throws an error', () => {
+        const file = '../fixtures/config/missing_resources_sample_config.yml';
+        const configFilePath = fileURLToPath(new URL(file, import.meta.url));
+
+        expect(() => ConfigLoader.fromFile(configFilePath)).toThrowError(
+          'Invalid config file: expected a top-level "resources" key.',
+        );
+      });
     });
   });
 });
