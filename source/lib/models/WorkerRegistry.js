64d13bcbcb44b2bc589f21b569a20b691431f064
Fernando Favini <darthjee@gmail.com>
2026-03-14 08:03:52 -0300

    Worker registry refactor (#27)

--- source/lib/models/WorkerRegistry.js
+++ source/lib/models/WorkerRegistry.js
@@ -15,7 +15,35 @@ class WorkerRegistry {
   constructor({ jobRegistry, workers }) {
     this.jobRegistry = jobRegistry;
     this.workersCount = workers;
-    this.workers = [];
+    this.workers = {};
+    this.busy = {};
+    this.idle = {};
+  }
+
+  /**
+   * Sets a worker as busy.
+   * @param {string} worker_id - The ID of the worker to set as busy.
+   */
+  setBusy(worker_id) {
+    const worker = this.workers[worker_id];
+
+    if (worker) {
+      delete this.idle[worker_id];
+      this.busy[worker_id] = worker;
+    }
+  }
+
+  /**
+   * Sets a worker as idle.
+   * @param {string} worker_id - The ID of the worker to set as idle.
+   */
+  setIdle(worker_id) {
+    const worker = this.workers[worker_id];
+
+    if (worker) {
+      delete this.busy[worker_id];
+      this.idle[worker_id] = worker;
+    }
   }
 
   /**
@@ -23,17 +51,27 @@ class WorkerRegistry {
    * @returns {Worker} The newly created Worker instance.
    */
   buildWorker() {
+    const id = this.#generateUUID();
+    const worker = new Worker({ id, jobRegistry: this.jobRegistry, workerRegistry: this });
+
+    this.workers[id] = worker;
+    this.idle[id] = worker;
+
+    return worker;
+  }
+
+  /**
+   * Generates a unique UUID that is not already assigned to any existing worker.
+   * @returns {string} A unique UUID string.
+   */
+  #generateUUID() {
     let id;
 
     do {
       id = randomUUID();
-    } while (this.workers.some(worker => worker.id === id));
+    } while (this.workers[id]);
 
-    const worker = new Worker({ id, jobRegistry: this.jobRegistry });
-
-    this.workers.push(worker);
-
-    return worker;
+    return id;
   }
 }
 
--- source/spec/models/WorkerRegistry_spec.js
+++ source/spec/models/WorkerRegistry_spec.js
@@ -4,6 +4,8 @@ import { WorkerRegistry } from '../../lib/models/WorkerRegistry.js';
 describe('WorkerRegistry', () => {
   let jobRegistry;
   let workerRegistry;
+  let worker;
+  let worker_id;
 
   beforeEach(() => {
     jobRegistry = new JobRegistry();
@@ -20,7 +22,7 @@ describe('WorkerRegistry', () => {
     });
 
     it('initializes an empty workers list', () => {
-      expect(workerRegistry.workers).toEqual([]);
+      expect(workerRegistry.workers).toEqual({});
     });
   });
 
@@ -34,7 +36,7 @@ describe('WorkerRegistry', () => {
     it('adds the worker to the list', () => {
       workerRegistry.buildWorker();
 
-      expect(workerRegistry.workers.length).toEqual(1);
+      expect(Object.keys(workerRegistry.workers).length).toEqual(1);
     });
 
     it('assigns the job registry to the worker', () => {
@@ -53,21 +55,81 @@ describe('WorkerRegistry', () => {
 
     describe('when called multiple times', () => {
       it('assigns unique ids to each worker', () => {
-        workerRegistry.buildWorker();
-        workerRegistry.buildWorker();
+        const worker1 = workerRegistry.buildWorker();
+        const worker2 = workerRegistry.buildWorker();
 
-        const ids = workerRegistry.workers.map(worker => worker.id);
-
-        expect(ids[0]).not.toEqual(ids[1]);
+        expect(workerRegistry.workers[worker1.id]).toEqual(worker1);
+        expect(workerRegistry.workers[worker2.id]).toEqual(worker2);
       });
 
       it('adds all workers to the list', () => {
         workerRegistry.buildWorker();
         workerRegistry.buildWorker();
 
-        expect(workerRegistry.workers.length).toEqual(2);
+        expect(Object.keys(workerRegistry.workers).length).toEqual(2);
       });
     });
 
+  });
+
+  describe('WorkerRegistry#setBusy', () => {
+    beforeEach(() => {
+      worker = workerRegistry.buildWorker();
+      worker_id = worker.id;
+    });
+
+    it('moves a worker from idle to busy when the worker exists', () => {
+      expect(workerRegistry.idle[worker_id]).toBe(worker);
+
+      workerRegistry.setBusy(worker_id);
+
+      expect(workerRegistry.busy[worker_id]).toBe(worker);
+      expect(workerRegistry.idle[worker_id]).toBeUndefined();
+    });
+
+    it('does nothing when the worker id does not exist', () => {
+      workerRegistry.setBusy('non-existent-id');
+
+      expect(Object.keys(workerRegistry.busy).length).toBe(0);
+    });
+
+    it('is idempotent when called multiple times for the same worker', () => {
+      workerRegistry.setBusy(worker_id);
+      workerRegistry.setBusy(worker_id);
+
+      expect(workerRegistry.busy[worker_id]).toBe(worker);
+      expect(workerRegistry.idle[worker_id]).toBeUndefined();
+    });
+  });
+
+  describe('WorkerRegistry#setIdle', () => {
+    beforeEach(() => {
+      worker = workerRegistry.buildWorker();
+      worker_id = worker.id;
+      workerRegistry.setBusy(worker_id);
+    });
+
+    it('moves a worker from busy to idle when the worker exists', () => {
+      expect(workerRegistry.busy[worker_id]).toBe(worker);
+
+      workerRegistry.setIdle(worker_id);
+
+      expect(workerRegistry.idle[worker_id]).toBe(worker);
+      expect(workerRegistry.busy[worker_id]).toBeUndefined();
+    });
+
+    it('does nothing when the worker id does not exist', () => {
+      workerRegistry.setIdle('non-existent-id');
+
+      expect(Object.keys(workerRegistry.idle).length).toBe(0);
+    });
+
+    it('is idempotent when called multiple times for the same worker', () => {
+      workerRegistry.setIdle(worker_id);
+      workerRegistry.setIdle(worker_id);
+
+      expect(workerRegistry.idle[worker_id]).toBe(worker);
+      expect(workerRegistry.busy[worker_id]).toBeUndefined();
+    });
   });
 });
