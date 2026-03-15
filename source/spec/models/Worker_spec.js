c7abcee0eab15ce957244086688347e1d9a0cca5
darthjee <darthjee@gmail.com>
2026-03-15 18:56:57 -0300

    Update worker specs

--- source/spec/models/Worker_spec.js
+++ source/spec/models/Worker_spec.js
@@ -1,22 +1,29 @@
 import { Worker } from '../../lib/models/Worker.js';
 import { JobRegistry } from '../../lib/registry/JobRegistry.js';
+import { WorkersRegistry } from '../../lib/registry/WorkersRegistry.js';
 
 describe('Worker', () => {
   let jobRegistry;
+  let workerRegistry;
   let worker;
 
   beforeEach(() => {
     jobRegistry = new JobRegistry();
-    worker = new Worker({ id: 1, jobRegistry });
+    workerRegistry = new WorkersRegistry({ quantity: 0, jobRegistry });
+    worker = new Worker({ id: 1, jobRegistry, workerRegistry });
   });
 
   describe('#constructor', () => {
+    it('stores the id', () => {
+      expect(worker.id).toEqual(1);
+    });
+
     it('stores the job registry', () => {
       expect(worker.jobRegistry).toEqual(jobRegistry);
     });
 
-    it('stores the id', () => {
-      expect(worker.id).toEqual(1);
+    it('stores the worker registry', () => {
+      expect(worker.workerRegistry).toEqual(workerRegistry);
     });
   });
 });
