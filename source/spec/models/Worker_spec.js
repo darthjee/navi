8e42d5da83fbbba43415ac06d7dd7295a714c238
Fernando Favini <darthjee@gmail.com>
2026-03-15 19:02:03 -0300

    Workers allocator (#39)

--- source/spec/models/Worker_spec.js
+++ source/spec/models/Worker_spec.js
@@ -1,22 +1,38 @@
+import { Job } from '../../lib/models/Job.js';
 import { Worker } from '../../lib/models/Worker.js';
 import { JobRegistry } from '../../lib/registry/JobRegistry.js';
+import { WorkersRegistry } from '../../lib/registry/WorkersRegistry.js';
 
 describe('Worker', () => {
   let jobRegistry;
+  let workerRegistry;
   let worker;
 
   beforeEach(() => {
     jobRegistry = new JobRegistry();
-    worker = new Worker({ id: 1, jobRegistry });
+    workerRegistry = new WorkersRegistry({ quantity: 0, jobRegistry });
+    worker = new Worker({ id: 1, jobRegistry, workerRegistry });
   });
 
   describe('#constructor', () => {
+    it('stores the id', () => {
+      expect(worker.id).toEqual(1);
+    });
+
     it('stores the job registry', () => {
       expect(worker.jobRegistry).toEqual(jobRegistry);
     });
 
-    it('stores the id', () => {
-      expect(worker.id).toEqual(1);
+    it('stores the worker registry', () => {
+      expect(worker.workerRegistry).toEqual(workerRegistry);
+    });
+  });
+
+  describe('#assign', () => {
+    it('assigns a job to the worker', () => {
+      const job = new Job({ payload: { value: 1 } });
+      worker.assign(job);
+      expect(worker.job).toEqual(job);
     });
   });
 });
