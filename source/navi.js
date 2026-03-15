65ab3571345e37151fe6b4691d8e522e6ea01338
darthjee <darthjee@gmail.com>
2026-03-15 15:20:25 -0300

    Lint fix

--- source/navi.js
+++ source/navi.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import { ArgumentsParser } from './lib/services/ArgumentsParser.js';
 import { Application } from './lib/services/Application.js';
+import { ArgumentsParser } from './lib/services/ArgumentsParser.js';
 
 const { configFile } = ArgumentsParser.parse(process.argv.slice(2));
 const app = new Application();
