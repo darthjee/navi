a439ec37e035141136e79a22b9ed41b6e6a0fcb8
darthjee <darthjee@gmail.com>
2026-03-18 20:03:03 -0300

    Add docs

--- source/lib/utils/IdentifyableCollection.js
+++ source/lib/utils/IdentifyableCollection.js
@@ -3,38 +3,75 @@ import { randomUUID } from 'crypto';
 class IdentifyableCollection {
   #items;
 
+  /**
+   * @param {object} items - Optional initial items, keyed by id.
+   */
   constructor(items = {}) {
     this.#items = items;
   }
 
+  /**
+   * Adds an item to the collection.
+   * @param {object} item - The item to add, must have a unique id.
+   */
   push(item) {
     this.#items[item.id] = item;
   }
 
+  /**
+   * Removes an item from the collection by id.
+   * @param {string|number} id - The id of the item to remove.
+   */
   remove(id) {
     delete this.#items[id];
   }
 
+  /**
+   * Retrieves an item from the collection by id.
+   * @param {string} id - The id of the item to retrieve.
+   * @returns {object | undefined} - The item if found, otherwise undefined.
+   */
   get(id) {
     return this.#items[id];
   }
 
-  byIndex(id) {
-    return this.list()[id];
+  /**
+   * Retrieves an item from the collection by index.
+   * @param {number} index - The index of the item to retrieve.
+   * @returns {object | undefined} - The item if found, otherwise undefined.
+   */
+  byIndex(index) {
+    return this.list()[index];
   }
 
+  /**
+   * Retrieves all items in the collection.
+   * @returns {object[]} - An array of all items in the collection.
+   */
   list() {
     return Object.values(this.#items);
   }
 
+  /**
+   * Retrieves the number of items in the collection.
+   * @returns {number} - The number of items in the collection.
+   */
   size() {
     return this.list().length;
   }
 
+  /**
+   * Checks if the collection has any items.
+   * @returns {boolean} - True if the collection has any items, false otherwise.
+   */
   hasAny() {
     return this.size() > 0;
   }
 
+  /**
+   * Generates a unique identifier.
+   * @returns {string} - A unique id not already present in the collection.
+   */
   generateUUID() {
     let id;
 
