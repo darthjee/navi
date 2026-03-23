20e95818c4f2e5d9105c62623450b21de108d9e9
darthjee <darthjee@gmail.com>
2026-03-23 18:06:01 -0300

    Add clients to factory

--- source/lib/factories/JobFactory.js
+++ source/lib/factories/JobFactory.js
@@ -8,14 +8,18 @@ import { IdGenerator } from '../utils/IdGenerator.js';
  * @author darthjee
  */
 class JobFactory extends Factory {
+  #clients;
+
   /**
    * Creates a new JobFactory instance with default settings for Job creation.
    * @param {object} options - Configuration options for the factory.
    * @param {class} options.klass - The class to instantiate (default is Job).
    * @param {object} options.attributesGenerator - The generator for unique attributes (default is IdGenerator).
+   * @param {ClientRegistry} options.clients - The clients registry to be used in created Job instances.
    */
-  constructor({ klass = Job, attributesGenerator = new IdGenerator() } = {}) {
+  constructor({ klass = Job, attributesGenerator = new IdGenerator(), clients } = {}) {
     super({ klass, attributesGenerator });
+    this.#clients = clients;
   }
 }
 
