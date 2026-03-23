73046c872f2565de8f671f8b465f6f7f96a84493
darthjee <darthjee@gmail.com>
2026-03-23 17:31:04 -0300

    Add client

--- source/lib/models/Job.js
+++ source/lib/models/Job.js
@@ -5,6 +5,7 @@
 class Job {
   #resourceRequest;
   #parameters;
+  #client;
 
   /**
    * Creates a new Job instance.
@@ -12,6 +13,7 @@ class Job {
    * @param {string} params.id - The unique identifier for this job.
    * @param {ResourceRequest} params.resourceRequest - The resource request associated with this job.
    * @param {object} params.parameters - Additional parameters for the request.
+   * @param {ClientRegistry} params.clients - Clients registry to be used in a request
    */
   constructor({ id, resourceRequest, parameters, clients }) {
     this.id = id;
@@ -21,6 +23,14 @@ class Job {
   }
 
   perform() {
+  }
+
+  client() {
+    if (! this.#client) {
+      this.#client = this.clients.getClient(this.#resourceRequest.clientName);
+    }
+
+    return this.#client;
   }
 }
 
