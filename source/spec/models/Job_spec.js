55ce515fca88b86fe92844f0f14145d3095cd8e3
darthjee <darthjee@gmail.com>
2026-03-23 17:41:45 -0300

    Prepare the test for perform

--- source/spec/models/Job_spec.js
+++ source/spec/models/Job_spec.js
@@ -1,11 +1,34 @@
 import { Job } from '../../lib/models/Job.js';
+import { ResourceRequest } from '../../lib/models/ResourceRequest.js';
+import { ClientRegistry } from '../../lib/registry/ClientRegistry.js';
+import { Client } from '../../lib/services/Client.js';
 
 describe('Job', () => {
+  let resourceRequest;
+  let clients;
+  let parameters;
+  let job;
+  let clientsMap;
+  let client;
+
+  beforeEach(() => {
+    resourceRequest = new ResourceRequest({ url: 'http://example.com', status: 200 });
+    client = new Client({ name: 'default' });
+    clientsMap = { default: client };
+    clients = new ClientRegistry(clientsMap);
+    parameters = {};
+    job = new Job({ id: 'id', resourceRequest, clients, parameters });
+  });
+
   describe('#constructor', () => {
     it('stores the id', () => {
-      const job = new Job({ id: 'id' });
-
       expect(job.id).toEqual('id');
+    });
+  });
+
+  describe('#process', () => {
+    xit('performs the job', () => {
+      expect(job.perform()).toBeUndefined();
     });
   });
 });
