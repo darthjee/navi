2c4ff1a7a76ac6b50baa026d1b2415d1255854dd
darthjee <darthjee@gmail.com>
2026-03-24 20:54:22 -0300

    Add first check

--- source/spec/services/Engine_spec.js
+++ source/spec/services/Engine_spec.js
@@ -1,7 +1,7 @@
-import { Engine } from '../../lib/services/Engine.js';
-import { DummyJobFactory } from '../support/factories/DummyJobFactory.js';
 import { JobRegistry } from '../../lib/registry/JobRegistry.js';
 import { WorkersRegistry } from '../../lib/registry/WorkersRegistry.js';
+import { Engine } from '../../lib/services/Engine.js';
+import { DummyJobFactory } from '../support/factories/DummyJobFactory.js';
 
 describe('Engine', () => {
   let engine;
@@ -13,8 +13,18 @@ describe('Engine', () => {
     jobFactory = new DummyJobFactory();
     jobRegistry = new JobRegistry({ factory: jobFactory });
 
-    workersRegistry = new WorkersRegistry();
+    workersRegistry = new WorkersRegistry({ quantity: 1 });
+    workersRegistry.initWorkers();
 
     engine = new Engine({ jobRegistry, workersRegistry });
+  });
+
+  describe('start', () => {
+    describe('when there are jobs to process and idle workers', () => {
+      it('should process jobs until there are no more jobs or idle workers', () => {
+        engine.start();
+        expect(jobRegistry.hasJob()).toBeFalse();
+      });
+    });
   });
 });
