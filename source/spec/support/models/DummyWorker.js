992ab97fbd752f835a08227a4fa40c829b627f7e
darthjee <darthjee@gmail.com>
2026-03-24 21:18:36 -0300

    Add dummy worker

---
+++ source/spec/support/models/DummyWorker.js
@@ -0,0 +1,21 @@
+import { Worker } from '../../../lib/models/Worker.js';
+
+class DummyWorker extends Worker {
+  perform() {
+    if (!this.job) {
+      throw new Error('No job assigned to worker');
+    }
+
+    try {
+      this.job.perform();
+      this.jobRegistry.finish(this.job);
+    } catch (error) {
+      this.jobRegistry.fail(this.job);
+    } finally {
+      this.job = undefined;
+      this.workerRegistry.setIdle(this.id);
+    }
+  }
+}
+
+export { DummyWorker };
