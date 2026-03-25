5cdf14284fb5a2df5f675183d43561f1e4197e35
darthjee <darthjee@gmail.com>
2026-03-24 21:00:55 -0300

    Test 1 job per worker

--- source/spec/services/Engine_spec.js
+++ source/spec/services/Engine_spec.js
@@ -2,6 +2,7 @@ import { JobRegistry } from '../../lib/registry/JobRegistry.js';
 import { WorkersRegistry } from '../../lib/registry/WorkersRegistry.js';
 import { Engine } from '../../lib/services/Engine.js';
 import { DummyJobFactory } from '../support/factories/DummyJobFactory.js';
+import { IdentifyableCollection } from '../../lib/utils/IdentifyableCollection.js';
 
 describe('Engine', () => {
   let engine;
@@ -9,20 +10,36 @@ describe('Engine', () => {
   let jobRegistry;
   let workersRegistry;
 
+  let finished;
+
   beforeEach(() => {
     jobFactory = new DummyJobFactory();
-    jobRegistry = new JobRegistry({ factory: jobFactory });
+    finished = new IdentifyableCollection();
+    jobRegistry = new JobRegistry({ finished, factory: jobFactory });
 
-    workersRegistry = new WorkersRegistry({ quantity: 1 });
+    workersRegistry = new WorkersRegistry({ quantity: 2 });
     workersRegistry.initWorkers();
 
     engine = new Engine({ jobRegistry, workersRegistry });
   });
 
   describe('start', () => {
-    describe('when there are jobs to process and idle workers', () => {
+    describe('when there are no jobs to process', () => {
       it('does nothing', () => {
         expect(jobRegistry.hasJob()).toBeFalse();
+        engine.start();
+        expect(jobRegistry.hasJob()).toBeFalse();
+      });
+    });
+
+    describe('when there are jobs to process and no idle workers', () => {
+      beforeEach(() => {
+        jobRegistry.enqueue({ resourceRequest: {}, parameters: {} });
+        jobRegistry.enqueue({ resourceRequest: {}, parameters: {} });
+      });
+
+      it('does nothing', () => {
+        expect(jobRegistry.hasJob()).toBeTrue();
         engine.start();
         expect(jobRegistry.hasJob()).toBeFalse();
       });
