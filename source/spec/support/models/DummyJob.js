485d2638257f41cb05447063c045a6d5886bad31
darthjee <darthjee@gmail.com>
2026-03-25 08:00:08 -0300

    Add success rate to job

--- source/spec/support/models/DummyJob.js
+++ source/spec/support/models/DummyJob.js
@@ -1,7 +1,19 @@
 import { Job } from '../../../lib/models/Job.js';
 
 class DummyJob extends Job {
+
+  static setSuccessRate(rate) {
+    this._successRate = rate;
+  }
+
+  static getSuccessRate() {
+    return this._successRate ?? 1;
+  }
+
   async perform() {
+    if (Math.random() > DummyJob.getSuccessRate()) {
+      throw new Error('Job failed');
+    }
   }
 }
 
