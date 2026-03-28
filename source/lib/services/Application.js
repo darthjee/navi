00f5535e7fefb54c2a1d4565fd4fc87220127efb
darthjee <darthjee@gmail.com>
2026-03-27 22:53:51 -0300

    Enqueue workers

--- source/lib/services/Application.js
+++ source/lib/services/Application.js
@@ -46,6 +46,7 @@ class Application {
 
   run() {
     this.engine = this.buildEngine();
+    this.enqueueFirstJobs();
     this.engine.start();
   }
 
@@ -59,7 +60,7 @@ class Application {
   enqueueFirstJobs() {
     this.config.resourceRegistry.filter(resource => true).forEach(resource => {
       resource.resourceRequests.forEach(resourceRequest => {
-        parameters = {};
+        let parameters = {};
         this.jobRegistry.enqueue({ resourceRequest, parameters });
       });
     });
