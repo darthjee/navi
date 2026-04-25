f3263e01932db2676a2646440b29a5b1f618021e
Fernando Favini <darthjee@gmail.com>
2026-04-24 22:33:31 -0300

    Fix dev app frontend proxy configuration (#345)

--- dev/proxy/rules/frontend.php
+++ dev/proxy/rules/frontend.php
@@ -3,21 +3,27 @@
 use Tent\Configuration;
 
 Configuration::buildRule([
-    'handler' => [
-        'type' => 'static_file',
-        'folder' => '/var/www/html/configuration/static'
-    ],
-    'matchers' => [
-        ['method' => 'GET', 'uri' => '/', 'type' => 'begins_with']
+  'handler' => [
+    'type' => 'static',
+    'location' => '/var/www/html/configuration/static'
+  ],
+  'matchers' => [
+    ['method' => 'GET', 'uri' => '/', 'type' => 'exact'],
+  ],
+  "middlewares" => [
+    [
+      'class' => 'Tent\Middlewares\SetPathMiddleware',
+      'path' => '/index.html'
     ]
+  ]
 ]);
 
 Configuration::buildRule([
-    'handler' => [
-        'type' => 'fixed_file',
-        'file' => '/var/www/html/configuration/static/index.html'
-    ],
-    'matchers' => [
-        ['method' => 'GET', 'uri' => '/', 'type' => 'begins_with']
-    ]
+  'handler' => [
+    'type' => 'static',
+    'location' => '/var/www/html/configuration/static'
+  ],
+  'matchers' => [
+    ['method' => 'GET', 'uri' => '/', 'type' => 'begins_with']
+  ]
 ]);
